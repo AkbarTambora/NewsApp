6f31fb20d83bb418723e800274b977c5800116e9
RakaiTambora <akbar.khaerullah.9@gmail.com>
2024-04-27 12:35:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/data/remote/retrofit/ApiService.kt
@@ -0,0 +1,12 @@
+package com.example.newsapp.data.remote.retrofit
+
+import com.example.newsapp.data.remote.response.NewsResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    //@GET("top-headlines?country=id&category=science")
+    @GET("everything?q=")
+    fun getNews(@Query("apiKey") apiKey: String): Call<NewsResponse>
+}
---
+++ app/src/main/java/com/example/newsapp/ui/NewsViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.newsapp.ui
+
+import androidx.lifecycle.ViewModel
+import com.example.newsapp.data.local.entity.NewsEntity
+import com.example.newsapp.data.source.NewsRepository
+
+class NewsViewModel(private val newsRepository: NewsRepository) : ViewModel() {
+    fun getHeadlineNews() = newsRepository.getHeadlineNews()
+
+    fun getBookmarkedNews() = newsRepository.getBookmarkedNews()
+
+    fun saveNews(news: NewsEntity) {
+        newsRepository.setBookmarkedNews(news, true)
+    }
+
+    fun deleteNews(news: NewsEntity) {
+        newsRepository.setBookmarkedNews(news, false)
+    }
+}
